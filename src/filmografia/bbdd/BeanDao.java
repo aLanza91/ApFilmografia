2ce09895a1eb74dbef802c6b226f16d7a2412a01
aLanza91 <alanza.1991@gmail.com>
2017-01-29 16:41:23 +0100

    BeanDao preparado sin métodos

--- src/filmografia/bbdd/BeanDao.java
+++ src/filmografia/bbdd/BeanDao.java
@@ -1,5 +1,49 @@
 package filmografia.bbdd;
 
-public class BeanDao {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+public class BeanDao {
+	
+	/**
+	 * Información de la base de datos
+	 */
+	private DataSource dscine;
+
+	/**
+	 * Representa la conexión con la base de datos
+	 */
+	private Connection conexion = null;
+
+	/**
+	 * Constructor que recibe el DataSource
+	 * @param dsBdValidaLogin
+	 */
+	public BeanDao(DataSource dscine) {
+		this.dscine = dscine;
+	}
+
+	/**
+	 * Proceso de conexión con la base de datos
+	 * @return
+	 * @throws SQLException
+	 */
+	public Connection getConexion() throws SQLException {
+		conexion = dscine.getConnection();
+		return conexion;
+	}
+
+	/**
+	 * Proceso que cierra la conexión con la base de datos
+	 * @throws SQLException
+	 */
+	public void close() throws SQLException {
+		if (conexion != null) {
+			conexion.close();
+		}
+		conexion = null;
+	}	
+	
 }
