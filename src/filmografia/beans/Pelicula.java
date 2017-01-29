8d18539c9b1d3f0fd202a4c90401fd3b3c203e32
aLanza91 <alanza.1991@gmail.com>
2017-01-29 17:10:30 +0100

    Bean Pelicula

---
+++ src/filmografia/beans/Pelicula.java
@@ -0,0 +1,54 @@
+package filmografia.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Pelicula implements Serializable{
+
+	private String director;
+	private String titulo;
+	private Date fecha;
+	/**
+	 * Constructor por defecto
+	 */
+	public Pelicula(){
+		this.director = "";
+		this.titulo = "";
+		this.fecha = new Date();
+	}
+	/**
+	 * Constructor con parametros
+	 * @param director
+	 * @param titulo
+	 * @param fecha
+	 */
+	public Pelicula(String director,String titulo, Date fecha){
+		this.director=director;
+		this.titulo=titulo;
+		this.fecha=fecha;
+	}
+	
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+}
