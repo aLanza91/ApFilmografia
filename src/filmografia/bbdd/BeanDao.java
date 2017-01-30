6ff687a3eab3a84e85541594b592855d516c884e
aLanza91 <alanza.1991@gmail.com>
2017-01-30 13:57:06 +0100

    BeanDao getPeliculas

--- src/filmografia/bbdd/BeanDao.java
+++ src/filmografia/bbdd/BeanDao.java
@@ -1,9 +1,16 @@
 package filmografia.bbdd;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+
+import filmografia.beans.BeanError;
+import filmografia.beans.BeanPelicula;
 
 public class BeanDao {
 	
@@ -45,5 +52,26 @@ public class BeanDao {
 		}
 		conexion = null;
 	}	
+	
+	public ArrayList<BeanPelicula> getPeliculas(HttpServletRequest request) throws SQLException, BeanError{
+		BeanPelicula pelicula = null;
+		ArrayList<BeanPelicula> listaPeliculas = new ArrayList<BeanPelicula>();
+		String director = request.getParameter("director");
+		Statement st = conexion.createStatement();
+		ResultSet rs = st.executeQuery("select director,titulo,fecha from login where director = '" + director + "'");
+		if (rs.next()){
+			while(rs.next()){
+				pelicula = new BeanPelicula (rs.getString("director"),rs.getString("titulo"),rs.getDate("fecha"));
+				listaPeliculas.add(pelicula);
+			}
+		}else{
+			throw new BeanError(3, "El director "+director+" no tiene ninguna pelicula registrada en la base de datos");
+		}
+		if (st!=null){
+			close();
+		}
+		
+		return listaPeliculas;
+	}
 	
 }
